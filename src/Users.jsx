66bd238d391f1eab08befda5f7623a37e02eeedc
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-05-08 17:53:04 +0600

    create a users

---
+++ src/Users.jsx
@@ -0,0 +1,40 @@
+import { useLoaderData } from "react-router-dom";
+
+const Users = () => {
+  const users = useLoaderData();
+  const handleDelete = _id => {
+    console.log(_id);
+    fetch(`http://localhost:9000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("deleted successfully");
+
+
+
+
+          
+          // const remaining = users.filter((user) => user._id !== _id);
+          // setUsers(remaining);
+        }
+      });
+  };
+  return (
+    <div>
+      <h1>{users.length}</h1>
+      <div>
+        {users.map((user) => (
+          <p key={user._id}>
+            {user.name} : {user.email}{" "}
+            <button onClick={() => handleDelete(user._id)}>X</button>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Users;
